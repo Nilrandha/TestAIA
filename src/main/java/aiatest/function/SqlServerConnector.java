45025d1c4f495390af34089310446c7805e47e97
Nilrandha Fernando <niluhz@gmail.com>
2025-07-01 17:25:40 +0530

    ENV Variable added

--- src/main/java/aiatest/function/SqlServerConnector.java
+++ src/main/java/aiatest/function/SqlServerConnector.java
@@ -12,16 +12,16 @@ public class SqlServerConnector {
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
 
             
-            String url = "jdbc:sqlserver://nilufreesqldbserver.database.windows.net:1433;"
-                       + "database=myFreeDB;"
-                       + "user=nilrandha@nilufreesqldbserver;"
-                       + "password=testaia@123;" 
-                       + "encrypt=true;"
-                       + "trustServerCertificate=false;"
-                       + "hostNameInCertificate=*.database.windows.net;"
-                       + "loginTimeout=30;";
+            // String url = "jdbc:sqlserver://nilufreesqldbserver.database.windows.net:1433;"
+            //            + "database=myFreeDB;"
+            //            + "user=nilrandha@nilufreesqldbserver;"
+            //            + "password=testaia@123;" 
+            //            + "encrypt=true;"
+            //            + "trustServerCertificate=false;"
+            //            + "hostNameInCertificate=*.database.windows.net;"
+            //            + "loginTimeout=30;";
 
-            //String url = System.getenv("DB_CONNECTION_STRING");
+            String url = System.getenv("DB_SETTEINGS");
              con = DriverManager.getConnection(url);
             // Establish connection
             con = DriverManager.getConnection(url);
